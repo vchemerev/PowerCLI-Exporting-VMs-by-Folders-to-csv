93439590713fe23d62bff8f0c7319f85e31a144e
Vladyslav Chemeriev <77836046+vchemerev@users.noreply.github.com>
2022-09-15 07:32:06 +0100

    Update Exporting_VMs_by_Folder_Name.ps1

--- Exporting_VMs_by_Folder_Name.ps1
+++ Exporting_VMs_by_Folder_Name.ps1
@@ -1,3 +1,12 @@
+#This script exports list of VMs with a full path from vcenter
+#As a result csv file will contain the next information
+# Name	DNS_Name	State	PowerState	Provisioned_Space Used_Space   Guest_OS	                            Memory_Size	CPUs	Up Time	                        IPAddress                                   FullPath
+# vm01	vm01.domain	green	PoweredOn	22.64 TB	      22.64 TB	   Red Hat Enterprise Linux 7 (64-bit)	256 GB	    20	    44 Days, 0 Hours, 56 Minutes	10.140.32.183|10.140.32.185|10.140.32.100|	folder1\folder2vm\final-folder\
+# vm02	vm01.domain	green	PoweredOn	29.13 TB	      29.13 TB	   Red Hat Enterprise Linux 7 (64-bit)	256 GB	    32	    43 Days, 1 Hours, 32 Minutes	10.140.32.182|10.140.32.99|10.140.32.98|	folder1\folder2vm\test\
+# vm03	vm01.domain	green	PoweredOn	194.08 GB	      136.29 GB	   Red Hat Enterprise Linux 7 (64-bit)	24 GB	    4	    43 Days, 0 Hours, 38 Minutes	10.140.32.86|192.168.0.29|169.254.54.106|	folder1\folder2vm\prod\
+#and so on
+
+
 Add-Type @"
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
@@ -55,7 +64,7 @@ $username = "user"
 $password = 'password'
 
 
-$foldername = "FolderName" # Folder name in vcenter to get VMs
+$foldername = "FolderName" # Folder name in vcenter to get VMs, if do not specify export list of all VMs from vcenter
 $reportdir = 'C:\Foldername\filename.csv' # Place for a file exporting
 
 
@@ -69,14 +78,14 @@ $vms_sum= $vms.Count
 $table=0
 $s=0
 
-# Exporting VMs by a specific  folder from vcenter
+# Exporting VMs list by a specific  folder from vcenter
    ForEach   ($vm in $VMs) {
    $s=$s+1
 
    
    $row = "" | Select Name,DNS_Name,State,PowerState,Provisioned_Space,Used_Space,Guest_OS,Memory_Size,CPUs,@{N="Up Time";E={$Timespan = New-Timespan -Seconds (Get-Stat -Entity $VM.Name -Stat sys.uptime.latest -Realtime -MaxSamples 1).Value
     "" + $Timespan.Days + " Days, "+ $Timespan.Hours + " Hours, " +$Timespan.Minutes + " Minutes"}},IPAddress,@{Name='FullPath';Expression={Get-VM $vm.Name | Get-VMFolderPath}}
-   echo "$s из $vms_sum"
+   echo "$s of $vms_sum"
    
    $row.Name = $vm.Name
 
